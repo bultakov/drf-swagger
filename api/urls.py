da343221028944bd62d60a5ff16b5181e09c516c
unknown <bii23.uz@gmail.com>
2023-03-25 12:22:27 +0500

    DRF Swagger

--- api/urls.py
+++ api/urls.py
@@ -13,7 +13,7 @@ from .views import (
 urlpatterns = [
     path("", home, name='home'),
 
-    path("news/", NewsListAPIView.as_view(), name="Newss"),
+    path("news/", NewsListAPIView.as_view(), name="news"),
     path("news/create/", NewsCreateAPIView.as_view(), name="create-news"),
     path("news/<int:pk>/", NewsDetailAPIView.as_view(), name="news"),
     path("news/update/<int:pk>/", NewsUpdateAPIView.as_view(), name="update-news"),
